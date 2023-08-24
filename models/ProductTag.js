a810f09ba8a50cdef1d7176a2fb6e04bbe49df8d
terenceDev <terence.arseneault@hotmail.com>
2023-08-24 08:00:10 -0300

    added link to demonstration video in readme and also fixed error when posting projuct, had } in wrong place in the ProductTags.js in my models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -20,15 +20,16 @@ ProductTag.init(
         model: "product",
         key: "id",
       },
-      tag_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "tag",
-          key: "id",
-        },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
       },
     },
   },
+
   {
     sequelize,
     timestamps: false,
